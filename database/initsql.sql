5028d8c366093fab09eef28516229b405da0983b
rootczy <365450906@qq.com>
2020-07-20 18:07:25 +0800

    增加注释说明

--- database/initsql.sql
+++ database/initsql.sql
@@ -1,1 +1,3 @@
+-- MySQL-dbName
+
 CREATE DATABASE IF NOT EXISTS hubers default charset utf8mb4 COLLATE utf8mb4_general_ci;
--- src/main/java/com/hubers/api/controller/CasesGroupAPIController.java
+++ src/main/java/com/hubers/api/controller/CasesGroupAPIController.java
@@ -19,6 +19,21 @@ import java.util.Map;
 public class CasesGroupAPIController {
     @Autowired
     CasesGroupApiService casesGroupApiService;
+    /**动态查询分组下用例字段key
+     * 获取分页数据，有查询条件，有排序API记录
+     * */
+    @GetMapping(value = "/api/key/list")
+    public List<CaseApiData> apiKeyList(CaseApiDataDTO caseApiDataDTO){
+        return  casesGroupApiService.findKey(caseApiDataDTO);
+    }
+
+    /**静态查询
+     * 根据关键字查询API记录
+     * */
+    @GetMapping(value = "/api/value/list")
+    public List<CaseApiData> apiValueList(Long groupId ,String value){
+        return casesGroupApiService.findValue(groupId,value);
+    }
 
     /**
      * 获取当前用例分组下的用例明细
@@ -26,22 +41,6 @@ public class CasesGroupAPIController {
     @GetMapping(value = "/api/list")
     public List<CaseApiData> apiList(Long groupId){
         return  casesGroupApiService.findCaseApiData(groupId);
-    }
-
-    /**静态查询
-     * 根据关键字查询
-     * */
-    @GetMapping(value = "/api/value/list")
-    public List<CaseApiData> apiValueList(Long groupId ,String value){
-        return casesGroupApiService.findValue(groupId,value);
-    }
-
-    /**动态查询分组下用例字段key
-     * 获取分页数据，有查询条件，有排序
-     * */
-    @GetMapping(value = "/api/key/list")
-    public List<CaseApiData> apiKeyList(CaseApiDataDTO caseApiDataDTO){
-        return  casesGroupApiService.findKey(caseApiDataDTO);
     }
 
     /**
@@ -82,6 +81,10 @@ public class CasesGroupAPIController {
         return casesGroupApiService.create(reqMap);
     }
 
+
+    /**
+     * 更新用例分组信息
+     * */
     @PutMapping(value = "/update")
     public CasesGroupApiData update(@Valid @RequestBody CasesGroupApiData casesGroupApiData , BindingResult result){
         if (result.hasErrors()){
@@ -89,6 +92,10 @@ public class CasesGroupAPIController {
         }
         return casesGroupApiService.update(casesGroupApiData);
     }
+
+    /**
+     * 删除用例分组信息
+     * */
     @DeleteMapping(value = "/delete/{id}")
     public String delete(@PathVariable Long id){
         return casesGroupApiService.delete(id);
